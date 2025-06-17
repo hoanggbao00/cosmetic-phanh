9050f5384b291d22f465058845a9bbad2435145a
hoanggbao00 <kaitou.kurosaki@gmail.com>
2025-06-17 11:13:03 +0700

    update blog table

--- app/admin/(blog)/blogs/blog-posts-table.tsx
+++ app/admin/(blog)/blogs/blog-posts-table.tsx
@@ -26,7 +26,7 @@ export default function BlogPostsTable() {
   const { mutate: deletePost } = useDeleteBlogPost()
 
   const onEditPost = (post: BlogPost) => {
-    router.push(`/admin/blogs/${post.slug}`)
+    router.push(`/admin/blogs/${post.id}`)
   }
 
   const columns: ColumnDef<BlogPost>[] = [
